346c09230c4df6c16f679fe4c7838bc927bfc408
Jongwoo Han <jongwooo.han@gmail.com>
2024-04-06 15:01:37 +0900

    Create reference/rotate/index_rule/rectangle/rotate_90.py

---
+++ reference/rotate/index_rule/rectangle/rotate_90.py
@@ -0,0 +1,9 @@
+arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+m = len(arr)
+n = len(arr[0])
+new_arr = [[0] * m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        new_arr[i][j] = arr[m - j - 1][i]
+for row in new_arr:
+    print(*row)
